838dd18fe737cfc736e87061a940b2292698226c
Malachie04 <malakimalachie@gmail.com>
2025-06-16 09:06:39 +0200

    First-Commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+//Selection éléments de la DOM
+
+const htmlContainer=document.querySelector('.container');
+
+//Ajouter du click
+
+htmlContainer.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+    let clickedElement=event.target;
+
+    //Selection de tous les enfants du selecteurs
+    let constElements=clickedElement.closest('.container');
+
+    
+    if(clickedElement.classList.contains('selected')){
+        //Déselection
+        clickedElement.classList.remove('selected');
+    }else{
+        constElements.querySelectorAll('button').forEach(element => {
+            element.classList.remove('selected');
+        });
+        //Ajout de la selection à la classe cible
+        clickedElement.classList.add('selected');
+    }
+
+    
+});
